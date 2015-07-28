b688408b39b3952d713003bcb5d0fe0203dbbc58
Rafael Monico <rmonico@moningrado>
2015-07-28 03:13:02 -0300

    Adding and removing conta table from database

--- src/main/java/zero/fluxodecaixa/Main.java
+++ src/main/java/zero/fluxodecaixa/Main.java
@@ -1,8 +1,24 @@
 package zero.fluxodecaixa;
 
+import java.sql.SQLException;
+
+import zero.fluxodecaixa.model.Conta;
+
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private final static String DATABASE_URL = "jdbc:sqlite:./database.sqlite";
 
+    public static void main(String[] args) throws SQLException {
+        JdbcConnectionSource connectionSource = new JdbcConnectionSource(DATABASE_URL);
+
+        DaoManager.createDao(connectionSource, Conta.class);
+
+        // if you need to create the table
+        TableUtils.dropTable(connectionSource, Conta.class, true);
+        TableUtils.createTable(connectionSource, Conta.class);
     }
 }
