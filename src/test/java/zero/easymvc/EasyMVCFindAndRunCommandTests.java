19d5272508493b3dae821fcc84e01ba621453d9e
Rafael Monico <rmonico@moningrado>
2015-07-27 23:31:44 -0300

    Refactor

--- src/test/java/zero/easymvc/EasyMVCFindAndRunCommandTests.java
+++ src/test/java/zero/easymvc/EasyMVCFindAndRunCommandTests.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class EasyMVCTests {
+public class EasyMVCFindAndRunCommandTests {
 
     public static class TestBean {
 
