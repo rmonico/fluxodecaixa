165d293501e505d00b090fbad74d2ce7fe595214
Rafael Monico <rmonico@moningrado>
2015-07-28 21:35:12 -0300

    Calling commands by controller on main

--- src/main/java/zero/fluxodecaixa/Main.java
+++ src/main/java/zero/fluxodecaixa/Main.java
@@ -2,8 +2,32 @@ package zero.fluxodecaixa;
 
 import java.sql.SQLException;
 
+import zero.easymvc.EasyMVC;
+import zero.easymvc.EasyMVCException;
+import zero.easymvc.StringArrayCommand;
+import zero.fluxodecaixa.app.ContaLsCommand;
+
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, EasyMVCException {
+        Main main = new Main();
+
+        main.run(args);
+    }
+
+    private EasyMVC controller;
+
+    private void run(String[] args) throws SQLException, EasyMVCException {
+        createAndSetupController();
+
+        controller.run(new StringArrayCommand(args));
+    }
+
+    private void createAndSetupController() throws SQLException {
+        controller = new EasyMVC();
+
+        controller.addDependencyManager(new ConnectionManager());
+        controller.registerCommandHandler(ContaLsCommand.class);
+        controller.bindPathToRenderer(ContaLsRenderer.class, new StringArrayCommand("conta", "ls"));
     }
 }
