d0e972dd1ae1c2392a54f4a40fdc9734b2a95138
Rafael Monico <rmonico@moningrado>
2015-07-29 20:45:20 -0300

    Tokenized params ok

--- src/test/java/zero/easymvc/TokenizedParameterTests.java
+++ src/test/java/zero/easymvc/TokenizedParameterTests.java
@@ -1,28 +1,40 @@
 package zero.easymvc;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 
 public class TokenizedParameterTests extends AbstractEasyMVCTest {
 
-    public static class Bean {
+    public static class Arguments {
         @FlagParameter(token = "--token")
         public boolean token;
     }
 
     public static class Command {
+
+        @ArgumentBean
+        @Bean
+        private Arguments arguments;
+
         @CommandHandler(path = { "command" })
-        public void execute(Bean bean) {
+        public void execute() {
 
         }
     }
 
     public static class Renderer {
 
+        @SuppressWarnings("unused")
+        private Arguments arguments;
+
         @zero.easymvc.Renderer
-        public void render(Bean bean) {
+        public void render() {
 
         }
     }
@@ -33,18 +45,35 @@ public class TokenizedParameterTests extends AbstractEasyMVCTest {
 
         controller.bindPathToRenderer(Renderer.class, new StringArrayCommand("command"));
 
-        Bean bean = (Bean) controller.run("command", "--token");
+        List<Object> beans = controller.run("command", "--token");
 
+        assertNotNull(beans);
+        assertEquals(1, beans.size());
+
+        assertEquals(Arguments.class, beans.get(0).getClass());
+
+        Arguments bean = (Arguments) beans.get(0);
+
+        assertNotNull(bean);
         assertTrue(bean.token);
     }
-    
+
     @Test
     public void should_run_without_optional_parameter() throws EasyMVCException {
         controller.registerCommandHandler(Command.class);
 
         controller.bindPathToRenderer(Renderer.class, new StringArrayCommand("command"));
 
-        Bean bean = (Bean) controller.run("command");
+        List<Object> beans = controller.run("command");
+
+        assertNotNull(beans);
+        assertEquals(1, beans.size());
+
+        assertEquals(Arguments.class, beans.get(0).getClass());
+
+        Arguments bean = (Arguments) beans.get(0);
+
+        assertNotNull(bean);
 
         assertFalse(bean.token);
     }
