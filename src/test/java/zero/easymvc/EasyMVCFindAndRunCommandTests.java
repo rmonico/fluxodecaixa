af9337d3064429a24ca0ab89df25aedeecde65b6
Rafael Monico <rmonico@moningrado>
2015-07-29 19:00:23 -0300

    Changed how Bean is injected into command

--- src/test/java/zero/easymvc/EasyMVCFindAndRunCommandTests.java
+++ src/test/java/zero/easymvc/EasyMVCFindAndRunCommandTests.java
@@ -1,6 +1,6 @@
 package zero.easymvc;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -8,26 +8,17 @@ import org.junit.Test;
 public class EasyMVCFindAndRunCommandTests extends EasyMVCTest {
 
     public static class TestBean {
-
         public boolean commandRan;
         public boolean rendererRan;
-        public static int instanceCount;
-
-        public TestBean() {
-            instanceCount++;
-            commandRan = false;
-            rendererRan = false;
-        }
-
-        public static void reset() {
-            instanceCount = 0;
-        }
-
     }
 
     public static class TestCommand {
+
+        @Bean
+        private TestBean bean;
+
         @CommandHandler(path = { "command", "subcommand" })
-        public void execute(TestBean bean) {
+        public void execute() {
             bean.commandRan = true;
         }
 
@@ -35,74 +26,38 @@ public class EasyMVCFindAndRunCommandTests extends EasyMVCTest {
 
     public static class TestRenderer {
 
-        public static TestBean bean;
+        @Bean
+        private TestBean bean;
 
         @Renderer
-        public void render(TestBean bean) {
-            TestRenderer.bean = bean;
+        public void render() {
             bean.rendererRan = true;
         }
     }
 
+    @Test
+    public void should_find_and_run_command() throws EasyMVCException {
+        controller.registerCommandHandler(TestCommand.class);
+
+        controller.bindPathToRenderer(TestRenderer.class, new StringArrayCommand("command", "subcommand"));
+
+        TestBean bean = (TestBean) controller.run("command", "subcommand");
+
+        assertNotNull(bean);
+        assertTrue(bean.commandRan);
+        assertTrue(bean.rendererRan);
+    }
+
     @Test(expected = EasyMVCException.class)
     public void should_throw_exception_on_invoking_unknown_command() throws EasyMVCException {
-        TestBean.reset();
-
         controller.run("unknown", "command");
     }
 
     @Test(expected = EasyMVCException.class)
     public void should_throw_exception_on_renderer_not_found() throws EasyMVCException {
-        TestBean.reset();
-
         controller.registerCommandHandler(TestCommand.class);
 
         controller.run("command", "subcommand");
-    }
-
-    public static class InvalidCommand {
-
-        @CommandHandler(path = "command")
-        public void run(Object firstBean, Object secondBean) {
-
-        }
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void command_should_receive_just_one_bean() throws EasyMVCException {
-        controller.registerCommandHandler(InvalidCommand.class);
-
-        controller.bindPathToRenderer(TestRenderer.class, new StringArrayCommand("command"));
-
-        controller.run("command");
-    }
-
-    public static class NoBeanCommand {
-
-        public static boolean ran;
-        public static boolean rendered;
-
-        @CommandHandler(path = "command")
-        public void run() {
-            ran = true;
-        }
-
-        @Renderer
-        public void render() {
-            rendered = true;
-        }
-    }
-
-    @Test
-    public void command_can_have_no_bean() throws EasyMVCException {
-        controller.registerCommandHandler(NoBeanCommand.class);
-
-        controller.bindPathToRenderer(NoBeanCommand.class, new StringArrayCommand("command"));
-
-        controller.run("command");
-
-        assertTrue(NoBeanCommand.ran);
-        assertTrue(NoBeanCommand.rendered);
     }
 
     @Test(expected = EasyMVCException.class)
@@ -112,18 +67,17 @@ public class EasyMVCFindAndRunCommandTests extends EasyMVCTest {
         controller.run("command");
     }
 
-    @Test
-    public void should_find_and_run_command() throws EasyMVCException {
-        TestBean.reset();
+    public static class InvalidCommand {
 
-        controller.registerCommandHandler(TestCommand.class);
+        @CommandHandler(path = "command")
+        public void run(Object parameter) {
 
-        controller.bindPathToRenderer(TestRenderer.class, new StringArrayCommand("command", "subcommand"));
-
-        controller.run("command", "subcommand");
-
-        assertTrue(TestRenderer.bean.commandRan);
-        assertTrue(TestRenderer.bean.rendererRan);
-        assertEquals(1, TestBean.instanceCount);
+        }
     }
+
+    @Test(expected = RuntimeException.class)
+    public void command_handler_cannot_have_params() throws EasyMVCException {
+        controller.registerCommandHandler(InvalidCommand.class);
+    }
+
 }
