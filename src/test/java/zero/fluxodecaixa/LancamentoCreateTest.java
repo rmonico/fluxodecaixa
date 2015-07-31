bb02fbbd13352a33757daf8a07b6d5b703641a2a
Rafael Monico <rmonico@moningrado>
2015-07-31 02:39:11 -0300

    Extracted before method, created a new test

--- src/test/java/zero/fluxodecaixa/LancamentoCreateTest.java
+++ src/test/java/zero/fluxodecaixa/LancamentoCreateTest.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import zero.easymvc.EasyMVCAssert;
@@ -18,12 +19,15 @@ public class LancamentoCreateTest extends FluxodecaixaTest {
         super("dbunit/contals_dataset.xml");
     }
 
-    @Test
-    public void should_create_a_lancamento_with_minimal_command_line() throws EasyMVCException {
+    @Before
+    public void before() {
         controller.registerCommandHandler(LancamentoCreateCommand.class);
 
         controller.registerRenderer(LancamentoCreateRenderer.class);
+    }
 
+    @Test
+    public void should_create_a_lancamento_with_minimal_command_line() throws EasyMVCException {
         List<Object> beans = controller.run("lanc", "add", "itau", "carteira", "1.99");
 
         EasyMVCAssert.assertBeanList(beans, 1);
@@ -33,5 +37,18 @@ public class LancamentoCreateTest extends FluxodecaixaTest {
         Calendar today = GregorianCalendar.getInstance();
 
         Assert.assertLancamento(today, "itau", "carteira", 1.99d, null, lancamento);
+    }
+
+    @Test
+    public void should_create_a_lancamento_with_observacao() throws EasyMVCException {
+        List<Object> beans = controller.run("lanc", "add", "itau", "carteira", "1.99", "--observacao=Note field on lancamento");
+
+        EasyMVCAssert.assertBeanList(beans, 1);
+
+        Lancamento lancamento = EasyMVCAssert.assertAndGetBean(beans, 0, Lancamento.class);
+
+        Calendar today = GregorianCalendar.getInstance();
+
+        Assert.assertLancamento(today, "itau", "carteira", 1.99d, "Note field on lancamento", lancamento);
     }
 }
