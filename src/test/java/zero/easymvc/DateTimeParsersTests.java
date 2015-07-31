d0ad849e142f02b23a78bc640c048dbb2c9613ab
Rafael Monico <rmonico@moningrado>
2015-07-30 23:18:07 -0300

    New class: TimeUtils

--- src/test/java/zero/easymvc/DateTimeParsersTests.java
+++ src/test/java/zero/easymvc/DateTimeParsersTests.java
@@ -3,16 +3,14 @@ package zero.easymvc;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.junit.Test;
 
-public class DateTimeParsersTests extends AbstractEasyMVCTest {
+import zero.utils.TimeUtils;
 
-    private SimpleDateFormat formatter;
+public class DateTimeParsersTests extends AbstractEasyMVCTest {
 
     public static class MyCommandArguments {
         @PositionalParameter(parser = TimestampParser.class)
@@ -58,43 +56,15 @@ public class DateTimeParsersTests extends AbstractEasyMVCTest {
 
         MyCommandArguments args = EasyMVCAssert.assertAndGetBean(beans, 0, MyCommandArguments.class);
 
-        formatter = new SimpleDateFormat("dd/MM/yyyy kk:mm:ss.SSS");
-
         assertNotNull(args.timestampParam);
-        Calendar expectedTimestamp = GregorianCalendar.getInstance();
-        setDateFields(expectedTimestamp, 1984, Calendar.JUNE, 8);
-        setTimeFields(expectedTimestamp, 17, 40, 12, 345);
-        assertEquals(timeToString(expectedTimestamp), timeToString(args.timestampParam));
+        assertEquals("08/06/1984 17:40:12.345", TimeUtils.timestampToString(args.timestampParam));
 
         assertNotNull(args.dateParam);
-        Calendar expectedDate = GregorianCalendar.getInstance();
-        setDateFields(expectedDate, 2015, Calendar.JULY, 30);
-        setTimeFields(expectedDate, 0, 0, 0, 0);
-        assertEquals(timeToString(expectedDate), timeToString(args.dateParam));
+        assertEquals("30/07/2015", TimeUtils.dateToString(args.dateParam));
 
         assertNotNull(args.timeParam);
-        Calendar expectedTime = GregorianCalendar.getInstance();
-        setDateFields(expectedTime, 1970, Calendar.JANUARY, 1);
-        setTimeFields(expectedTime, 20, 40, 0, 0);
-        assertEquals(timeToString(expectedTime), timeToString(args.timeParam));
+        assertEquals("20:40:00.000", TimeUtils.timeToString(args.timeParam));
 
-    }
-
-    private void setDateFields(Calendar expectedDate, int year, int month, int day) {
-        expectedDate.set(Calendar.YEAR, year);
-        expectedDate.set(Calendar.MONTH, month);
-        expectedDate.set(Calendar.DAY_OF_MONTH, day);
-    }
-
-    private void setTimeFields(Calendar expectedDate, int hour, int minute, int second, int miliseconds) {
-        expectedDate.set(Calendar.HOUR_OF_DAY, hour);
-        expectedDate.set(Calendar.MINUTE, minute);
-        expectedDate.set(Calendar.SECOND, second);
-        expectedDate.set(Calendar.MILLISECOND, miliseconds);
-    }
-
-    private String timeToString(Calendar expectedTimestamp) {
-        return formatter.format(expectedTimestamp.getTime());
     }
 
 }
---
+++ src/test/java/zero/utils/TimeUtils.java
@@ -0,0 +1,24 @@
+package zero.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class TimeUtils {
+
+    private static SimpleDateFormat timestampFormatter = new SimpleDateFormat("dd/MM/yyyy kk:mm:ss.SSS");
+    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+    private static SimpleDateFormat timeFormatter = new SimpleDateFormat("kk:mm:ss.SSS");
+
+    public static String timestampToString(Calendar timestamp) {
+        return timestampFormatter.format(timestamp.getTime());
+    }
+
+    public static String dateToString(Calendar date) {
+        return dateFormatter.format(date.getTime());
+    }
+
+    public static String timeToString(Calendar time) {
+        return timeFormatter.format(time.getTime());
+    }
+
+}
