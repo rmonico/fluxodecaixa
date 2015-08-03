cde73d0cb7393118e32b058fcb44f17653a785ee
Rafael Monico <rmonico@moningrado>
2015-08-03 19:43:43 -0300

    ConnectionManager handling connection object only

--- src/main/java/zero/fluxodecaixa/ConnectionManager.java
+++ src/main/java/zero/fluxodecaixa/ConnectionManager.java
@@ -3,12 +3,7 @@ package zero.fluxodecaixa;
 import java.sql.SQLException;
 
 import zero.easymvc.DependencyManager;
-import zero.fluxodecaixa.app.dao.AbstractDao;
-import zero.fluxodecaixa.app.dao.ContaDao;
-import zero.fluxodecaixa.app.dao.LancamentoDao;
-import zero.fluxodecaixa.app.dao.TransacaoDao;
 
-import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 
@@ -30,15 +25,12 @@ public class ConnectionManager implements DependencyManager {
 
     @Override
     public Class<?>[] managedClasses() {
-        return new Class<?>[] { ConnectionSource.class, ContaDao.class, TransacaoDao.class, LancamentoDao.class };
+        return new Class<?>[] { ConnectionSource.class };
     }
 
     @Override
     public Object getInstance(Class<?> dependencyClass) throws SQLException {
-        if (dependencyClass.isAssignableFrom(AbstractDao.class))
-            return DaoManager.createDao(connectionSource, dependencyClass);
-        else
-            return connectionSource;
+        return connectionSource;
     }
 
     @Override
