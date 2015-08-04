341bafd83ef06a183b83c5a8b273469fc5d8cd80
Rafael Monico <rmonico@moningrado>
2015-08-04 10:35:46 -0300

    Configuring commands in Main class

--- src/main/java/zero/fluxodecaixa/Main.java
+++ src/main/java/zero/fluxodecaixa/Main.java
@@ -2,11 +2,22 @@ package zero.fluxodecaixa;
 
 import java.sql.SQLException;
 
+import zero.easymvc.DependencyManager;
 import zero.easymvc.EasyMVC;
 import zero.easymvc.EasyMVCException;
 import zero.easymvc.StringArrayCommand;
+import zero.fluxodecaixa.app.ContaCreateCommand;
 import zero.fluxodecaixa.app.ContaListCommand;
+import zero.fluxodecaixa.app.ContaRemoveCommand;
+import zero.fluxodecaixa.app.LancamentoCreateCommand;
+import zero.fluxodecaixa.app.LancamentoListCommand;
+import zero.fluxodecaixa.app.LancamentoListRenderer;
+import zero.fluxodecaixa.renderer.ContaCreateRenderer;
 import zero.fluxodecaixa.renderer.ContaListRenderer;
+import zero.fluxodecaixa.renderer.ContaRemoveRenderer;
+import zero.fluxodecaixa.renderer.LancamentoCreateRenderer;
+
+import com.j256.ormlite.support.ConnectionSource;
 
 public class Main {
 
@@ -28,8 +39,22 @@ public class Main {
     private void createAndSetupController() throws SQLException {
         controller = new EasyMVC();
 
-        controller.addDependencyManager(new ConnectionManager());
+        ConnectionManager connectionManager = new ConnectionManager();
+
+        controller.addDependencyManager(connectionManager);
+
+        DependencyManager daoManager = new DaoManager((ConnectionSource) connectionManager.getInstance(ConnectionSource.class));
+        controller.addDependencyManager(daoManager);
+
+        controller.registerCommandHandler(ContaCreateCommand.class);
+        controller.registerRenderer(ContaCreateRenderer.class);
         controller.registerCommandHandler(ContaListCommand.class);
         controller.registerRenderer(ContaListRenderer.class);
+        controller.registerCommandHandler(ContaRemoveCommand.class);
+        controller.registerRenderer(ContaRemoveRenderer.class);
+        controller.registerCommandHandler(LancamentoCreateCommand.class);
+        controller.registerRenderer(LancamentoCreateRenderer.class);
+        controller.registerCommandHandler(LancamentoListCommand.class);
+        controller.registerRenderer(LancamentoListRenderer.class);
     }
 }
