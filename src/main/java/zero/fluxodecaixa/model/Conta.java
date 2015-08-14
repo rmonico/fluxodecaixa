3deec5f368a730d5d90d0f99a8f1366cebc39c83
Rafael Monico <rmonico@moningrado>
2015-08-14 11:16:35 -0300

    Conta class ready to be used as a map key

--- src/main/java/zero/fluxodecaixa/model/Conta.java
+++ src/main/java/zero/fluxodecaixa/model/Conta.java
@@ -45,4 +45,35 @@ public class Conta {
         this.contabilizavel = contabilizavel;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (contabilizavel ? 1231 : 1237);
+        result = prime * result + id;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Conta other = (Conta) obj;
+        if (contabilizavel != other.contabilizavel)
+            return false;
+        if (id != other.id)
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
 }
