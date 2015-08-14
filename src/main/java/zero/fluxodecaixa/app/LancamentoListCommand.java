fed967cdf4199e0a945ca2371da5d531593b5a56
Rafael Monico <rmonico@moningrado>
2015-08-14 11:16:15 -0300

    Creation of sorted by data query for lancamento moved to dao class

--- src/main/java/zero/fluxodecaixa/app/LancamentoListCommand.java
+++ src/main/java/zero/fluxodecaixa/app/LancamentoListCommand.java
@@ -10,10 +10,8 @@ import zero.easymvc.Dependency;
 import zero.fluxodecaixa.app.dao.LancamentoDao;
 import zero.fluxodecaixa.app.dao.TransacaoDao;
 import zero.fluxodecaixa.model.Lancamento;
-import zero.fluxodecaixa.model.Transacao;
 
 import com.j256.ormlite.stmt.PreparedQuery;
-import com.j256.ormlite.stmt.QueryBuilder;
 
 public class LancamentoListCommand {
 
@@ -28,23 +26,9 @@ public class LancamentoListCommand {
 
     @CommandHandler(path = { "lanc", "ls" })
     public void run() throws ParseException, SQLException {
-        PreparedQuery<Lancamento> query = createQuery();
+        PreparedQuery<Lancamento> query = dao.createQuerySortedByTransacaoData(transacaoDao);
 
         lancamentos = dao.query(query);
-    }
-
-    private PreparedQuery<Lancamento> createQuery() throws SQLException {
-        QueryBuilder<Transacao, Integer> transacaoBuilder = transacaoDao.queryBuilder();
-
-        transacaoBuilder.orderBy(Transacao.DATA_FIELD_NAME, true);
-
-        QueryBuilder<Lancamento, Integer> builder = dao.queryBuilder();
-
-        builder.join(transacaoBuilder);
-
-        PreparedQuery<Lancamento> query = builder.prepare();
-
-        return query;
     }
 
 }
--- src/main/java/zero/fluxodecaixa/app/dao/LancamentoDao.java
+++ src/main/java/zero/fluxodecaixa/app/dao/LancamentoDao.java
@@ -3,7 +3,10 @@ package zero.fluxodecaixa.app.dao;
 import java.sql.SQLException;
 
 import zero.fluxodecaixa.model.Lancamento;
+import zero.fluxodecaixa.model.Transacao;
 
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
 
 public class LancamentoDao extends AbstractDao<Lancamento> {
@@ -15,4 +18,19 @@ public class LancamentoDao extends AbstractDao<Lancamento> {
     public static LancamentoDao getInstance(ConnectionSource connection) throws SQLException {
         return (LancamentoDao) AbstractDao.getInstance(connection, Lancamento.class);
     }
+
+    public PreparedQuery<Lancamento> createQuerySortedByTransacaoData(TransacaoDao transacaoDao) throws SQLException {
+        QueryBuilder<Transacao, Integer> transacaoBuilder = transacaoDao.queryBuilder();
+
+        transacaoBuilder.orderBy(Transacao.DATA_FIELD_NAME, true);
+
+        QueryBuilder<Lancamento, Integer> builder = queryBuilder();
+
+        builder.join(transacaoBuilder);
+
+        PreparedQuery<Lancamento> query = builder.prepare();
+
+        return query;
+    }
+
 }
