af11411232ac78af47ab0defdb3d4ce7cfbdd04b
Rafael Monico <rmonico@moningrado>
2015-08-14 11:13:22 -0300

    SaldoTests fixed

--- src/test/java/zero/fluxodecaixa/SaldoTests.java
+++ src/test/java/zero/fluxodecaixa/SaldoTests.java
@@ -1,5 +1,7 @@
 package zero.fluxodecaixa;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 
 import org.junit.Test;
@@ -26,11 +28,13 @@ public class SaldoTests extends FluxodecaixaTest {
         @SuppressWarnings("unchecked")
         List<Saldo> saldos = EasyMVCAssert.assertAndGetBean(beans, 0, List.class);
 
-        Assert.assertSaldo("03/Ago/2015", "itau", "945", saldos.get(0));
-        Assert.assertSaldo("03/Ago/2015", "bradesco", "925", saldos.get(1));
+        assertEquals(4, saldos.size());
 
-        Assert.assertSaldo("04/Ago/2015", "itau", "835", saldos.get(2));
-        Assert.assertSaldo("04/Ago/2015", "bradesco", "833", saldos.get(3));
+        Assert.assertSaldo("03/Ago/2015", "bradesco", "925", saldos.get(0));
+        Assert.assertSaldo("03/Ago/2015", "itau", "945", saldos.get(1));
+
+        Assert.assertSaldo("04/Ago/2015", "bradesco", "833", saldos.get(2));
+        Assert.assertSaldo("04/Ago/2015", "itau", "835", saldos.get(3));
     }
 
 }
