88c40aa8afb0b25fb18218ca01255f47d76a277f
Rafael Monico <rmonico@moningrado>
2015-08-17 03:50:58 -0300

    New test for bug on Transacao.data format on database

--- src/test/java/zero/fluxodecaixa/SaldoTests.java
+++ src/test/java/zero/fluxodecaixa/SaldoTests.java
@@ -2,6 +2,8 @@ package zero.fluxodecaixa;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +13,7 @@ import org.junit.Test;
 import zero.easymvc.EasyMVCAssert;
 import zero.easymvc.EasyMVCException;
 import zero.fluxodecaixa.model.Saldo;
+import zero.utils.TimeUtils;
 
 public class SaldoTests extends FluxodecaixaTest {
 
@@ -43,6 +46,32 @@ public class SaldoTests extends FluxodecaixaTest {
         expectedValores.put("itau", "835");
 
         Assert.assertSaldo("04/Ago/2015", expectedValores, saldos.get(1));
+    }
+
+    @Test
+    public void should_put_saldos_of_today_together() throws EasyMVCException {
+        controller.run("lanc", "add", "itau", "casa", "100");
+        controller.run("lanc", "add", "salário", "itau", "50");
+
+        controller.run("lanc", "add", "bradesco", "casa", "80");
+        controller.run("lanc", "add", "salário", "bradesco", "130");
+
+        List<Object> beans = controller.run("saldo");
+
+        EasyMVCAssert.assertBeanList(beans, 1);
+
+        @SuppressWarnings("unchecked")
+        List<Saldo> saldos = EasyMVCAssert.assertAndGetBean(beans, 0, List.class);
+
+        assertEquals(3, saldos.size());
+
+        Map<String, String> expectedValores = new HashMap<String, String>();
+        expectedValores.put("bradesco", "883");
+        expectedValores.put("itau", "785");
+
+        Calendar today = GregorianCalendar.getInstance();
+
+        Assert.assertSaldo(TimeUtils.dateToString(today), expectedValores, saldos.get(2));
     }
 
 }
