7dd5786c81b9b0acf1a0c49a923bd37825e50880
Rafael Monico <rmonico@moningrado>
2015-08-19 05:54:52 -0300

    Showing transacao id on lancamento create

--- src/main/java/zero/fluxodecaixa/renderer/LancamentoCreateRenderer.java
+++ src/main/java/zero/fluxodecaixa/renderer/LancamentoCreateRenderer.java
@@ -2,6 +2,8 @@ package zero.fluxodecaixa.renderer;
 
 import zero.easymvc.Renderer;
 import zero.fluxodecaixa.model.Lancamento;
+import zero.listprinter.DoubleFormatter;
+import zero.utils.TimeUtils;
 
 public class LancamentoCreateRenderer {
 
@@ -9,11 +11,14 @@ public class LancamentoCreateRenderer {
 
     @Renderer(path = { "lanc", "add" })
     public void render() {
+        String data = TimeUtils.dateToString(lancamento.getTransacao().getData());
+        int transacaoId = lancamento.getTransacao().getId();
         String origem = lancamento.getOrigem().getNome();
         String destino = lancamento.getDestino().getNome();
-        double valor = lancamento.getValor();
+        String valor = DoubleFormatter.getInstance().format(lancamento.getValor());
+        String observacao = lancamento.getObservacao() != null ? String.format(" (%s)", lancamento.getObservacao()) : "";
 
-        String message = String.format("Lançamento \"%s -> %s (%f)\" criado com sucesso.", origem, destino, valor);
+        String message = String.format("OK: %s %s -> %s %s%s [transacao: #%d]", data, origem, destino, valor, observacao, transacaoId);
 
         System.out.println(message);
     }
