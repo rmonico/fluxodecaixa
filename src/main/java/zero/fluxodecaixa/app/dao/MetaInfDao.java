55b8c15a737177eb863e2ef85df38085d3e6c908
Rafael Monico <rmonico@moningrado>
2015-08-21 00:25:50 -0300

    MetaInfTests passing

--- src/main/java/zero/fluxodecaixa/app/dao/MetaInfDao.java
+++ src/main/java/zero/fluxodecaixa/app/dao/MetaInfDao.java
@@ -1,9 +1,13 @@
 package zero.fluxodecaixa.app.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import zero.fluxodecaixa.model.MetaInf;
 
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.Where;
 import com.j256.ormlite.support.ConnectionSource;
 
 public class MetaInfDao extends AbstractDao<MetaInf> {
@@ -16,7 +20,20 @@ public class MetaInfDao extends AbstractDao<MetaInf> {
         return (MetaInfDao) AbstractDao.getInstance(connection, MetaInf.class);
     }
 
-    public String getValue(String key) {
-        return null;
+    public String getValue(String key) throws SQLException {
+        QueryBuilder<MetaInf, Integer> builder = queryBuilder();
+
+        Where<MetaInf, Integer> where = builder.where();
+
+        where.eq(MetaInf.KEY_FIELD_NAME, key);
+
+        PreparedQuery<MetaInf> query = builder.prepare();
+
+        List<MetaInf> keys = query(query);
+
+        if (keys.isEmpty())
+            return null;
+
+        return keys.get(0).getValue();
     }
 }
