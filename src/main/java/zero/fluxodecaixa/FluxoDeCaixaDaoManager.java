9b2d6dda8db9272189263d4a76ee785848fc70b4
Rafael Monico <rmonico@moningrado>
2015-08-28 07:48:00 -0300

    DaoManager renamed to FluxoDeCaixaDaoManager

---
+++ src/main/java/zero/fluxodecaixa/FluxoDeCaixaDaoManager.java
@@ -0,0 +1,28 @@
+package zero.fluxodecaixa;
+
+import java.util.Map;
+
+import com.j256.ormlite.support.ConnectionSource;
+
+import zero.easymvc.ormlite.DaoManager;
+import zero.fluxodecaixa.app.dao.ContaDao;
+import zero.fluxodecaixa.app.dao.LancamentoDao;
+import zero.fluxodecaixa.app.dao.TransacaoDao;
+import zero.fluxodecaixa.model.Conta;
+import zero.fluxodecaixa.model.Lancamento;
+import zero.fluxodecaixa.model.Transacao;
+
+public class FluxoDeCaixaDaoManager extends DaoManager {
+
+    public FluxoDeCaixaDaoManager(ConnectionSource connection) {
+        super(connection);
+    }
+
+    @Override
+    protected void populateDaoInfo(Map<Class<?>, Class<?>> daoInfo) {
+        daoInfo.put(ContaDao.class, Conta.class);
+        daoInfo.put(TransacaoDao.class, Transacao.class);
+        daoInfo.put(LancamentoDao.class, Lancamento.class);
+    }
+
+}
