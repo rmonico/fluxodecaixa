2065fa7ff493cc6b1cd1dd7cdfbfcffac4b407c6
Rafael Monico <rmonico@moningrado>
2015-09-18 06:30:02 -0300

    Using default connection string

--- src/test/java/zero/fluxodecaixa/FluxodecaixaApplicationFactoryTests.java
+++ src/test/java/zero/fluxodecaixa/FluxodecaixaApplicationFactoryTests.java
@@ -5,16 +5,18 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import zero.environment.ApplicationPropertyKeys;
+import zero.environment.Environment;
 
 public class FluxodecaixaApplicationFactoryTests extends FluxodecaixaTest {
 
     @Test
     public void should_default_properties_be_set() {
-        assertEquals(FluxodecaixaTestApplicationFactory.DEFAULT_TEST_JDBC_URL, appProperties.getProperty(ApplicationPropertyKeys.JDBC_URL_KEY));
-        assertEquals(FluxodecaixaTestApplicationFactory.DEFAULT_TEST_JDBC_DRIVER_CLASS, appProperties.getProperty(ApplicationPropertyKeys.JDBC_DRIVER_CLASS_KEY));
+        String appHomeFolder = Environment.get().getProperty(ApplicationPropertyKeys.APP_HOME_PROPERTY_KEY);
 
         assertEquals(FluxodecaixaApplicationFactory.EXECUTABLE_MAJOR_VERSION, appProperties.getProperty(ApplicationPropertyKeys.EXECUTABLE_MAJOR_VERSION_PROPERTY_KEY));
         assertEquals(FluxodecaixaApplicationFactory.EXECUTABLE_MINOR_VERSION, appProperties.getProperty(ApplicationPropertyKeys.EXECUTABLE_MINOR_VERSION_PROPERTY_KEY));
         assertEquals(FluxodecaixaApplicationFactory.EXECUTABLE_PROJECT_PHASE, appProperties.getProperty(ApplicationPropertyKeys.EXECUTABLE_PROJECT_PHASE_PROPERTY_KEY));
+        assertEquals(String.format("jdbc:sqlite:%s/database.sqlite", appHomeFolder), Environment.get().getProperty(ApplicationPropertyKeys.JDBC_URL_KEY));
+        assertEquals("org.sqlite.JDBC", Environment.get().getProperty(ApplicationPropertyKeys.JDBC_DRIVER_CLASS_KEY));
     }
 }
--- src/test/java/zero/fluxodecaixa/FluxodecaixaTestApplicationFactory.java
+++ src/test/java/zero/fluxodecaixa/FluxodecaixaTestApplicationFactory.java
@@ -1,15 +1,11 @@
 package zero.fluxodecaixa;
 
 import zero.easymvc.ormlite.DatabaseVersion;
-import zero.environment.ApplicationPropertyKeys;
 import zero.utils.test.DBUnitUpdater;
 
 import com.j256.ormlite.support.ConnectionSource;
 
 public class FluxodecaixaTestApplicationFactory extends FluxodecaixaApplicationFactory {
-
-    public static final String DEFAULT_TEST_JDBC_URL = "jdbc:sqlite:dbunit/test_database";
-    public static final String DEFAULT_TEST_JDBC_DRIVER_CLASS = "org.sqlite.JDBC";
 
     private String[] datasetFileNames;
 
@@ -17,12 +13,7 @@ public class FluxodecaixaTestApplicationFactory extends FluxodecaixaApplicationF
         this.datasetFileNames = datasetFileNames;
     }
 
-    @Override
-    protected void setupDefaultProperties() {
-        super.setupDefaultProperties();
-
-        properties.setProperty(ApplicationPropertyKeys.JDBC_URL_KEY, DEFAULT_TEST_JDBC_URL);
-        properties.setProperty(ApplicationPropertyKeys.JDBC_DRIVER_CLASS_KEY, DEFAULT_TEST_JDBC_DRIVER_CLASS);
+        this.datasetFileNames = datasetFileNames;
     }
 
     @Override
